c78ac2b548be82b5442d481eda52a54b3e6cd2c6
Alexander <Sanyi.Ponzel@mail.ru>
2020-12-16 13:36:25 +0700

    it2

--- src/js/moduls/calc.js
+++ src/js/moduls/calc.js
@@ -4,18 +4,82 @@ let calc = () => {
     let calc__checkbox = document.querySelectorAll(".calc__custom .calc__checkbox");
     let countResult = 0;
     let calc__item_chek = document.querySelectorAll(".calc__item .calc__ceck .calc__checkbox");
+
     let calc__wrapper__range = document.querySelectorAll(".calc__wrapper .calc__range");
     let calc__number = document.querySelectorAll(".calc__number");
+    let calc__ceck_range = document.querySelectorAll(".calc__ceck_range .calc__checkbox");
+
     let calc__modification__range = document.querySelector(".calc__modification .calc__mod .calc__range");
     let calc__number_bold = document.querySelector(".calc__number_bold");
     let calc__information_range = document.querySelectorAll(".calc__information .calc__range");
     let calc__information_check = document.querySelectorAll(".calc__information .calc__checkbox");
     let calc__numbers = document.querySelectorAll(".calc__numbers span");
     let calc__list_btn = document.querySelector(".calc__list img");
+    let calc__result_box = document.querySelector(".calc__result")
     let calc__estimate = document.querySelector(".calc__estimate");
     let calc__radiobox = document.querySelectorAll(".calc__radiobox");
-    let calc__ceck_range = document.querySelectorAll(".calc__ceck_range .calc__checkbox")
 
+    let calc__item_range = document.querySelectorAll(".calc__item_range .calc__range");
+    let calc__item_check = document.querySelectorAll(".calc__item_range .calc__checkbox");
+    let calc__item_number = document.querySelectorAll(".calc__item_range .calc__numbers");
+
+    let calc__сustomization_ceck = document.querySelectorAll(".calc__сustomization .calc__wrap .calc__checkbox");
+    let calc__name = document.querySelector(".calc__estimate");
+
+    let prevstate = calc__estimate.clientHeight;
+
+    var countStart = 0;
+    calc__сustomization_ceck.forEach(item => {
+        if(item.hasAttribute("disabled")){
+            countStart += +Number(item.value);
+            let name = item.dataset.name;
+            let newEl = document.createElement("div");
+            newEl.classList.add("calc__estimate_info")
+            newEl.innerHTML = `
+            <p>${name}</p>
+            <span>${item.value} тг</span>`;
+
+            calc__name.appendChild(newEl);
+        }
+        item.addEventListener("click", () => {
+            if(document.documentElement.clientWidth <= 991) {
+                return
+            }
+            else{
+                if(item.checked == true) {
+                    let name = item.dataset.name;
+                    let newEl = document.createElement("div");
+                    let id = item.getAttribute("id");
+                    newEl.classList.add("calc__estimate_info");
+                    newEl.setAttribute(`data-id`, `${id}`);
+                    newEl.innerHTML = `
+                    <p>${name}</p>
+                    <span>${item.value} тг</span>`;
+        
+                    calc__name.appendChild(newEl);
+                    calc__estimate.style.height = `${calc__estimate.clientHeight + 22}px`
+                    calc__result_box.style.height = `${calc__result_box.clientHeight + 22}px`
+                    prevstate = prevstate + 22;
+                }
+                else {
+                    let calc__estimate_info = document.querySelectorAll(".calc__estimate_info");
+                    let id = item.getAttribute("id")
+                    calc__estimate_info.forEach(count => {
+                        let dataId = count.dataset.id;
+                        if(dataId == id) {
+                            count.parentNode.removeChild(count);
+                            calc__estimate.style.height = `${calc__estimate.clientHeight - 22}px`
+                            calc__result_box.style.height = `${calc__result_box.clientHeight - 22}px`;
+                            prevstate = prevstate - 22;
+                        }
+                    })
+                }
+            }
+        })
+    })
+
+    countResult = countResult + countStart;
+    calc__result.innerHTML = `${countResult} тг`;
 
     calc__checkbox.forEach(item => {
         item.addEventListener("click", () => {
@@ -50,6 +114,64 @@ let calc = () => {
                     return;
                 }
             }
+
+            if(item.dataset.names){
+                if(document.documentElement.clientWidth <= 991) {
+                    return
+                }
+                else{
+                    if(item.checked == true) {
+                        let name = item.dataset.names;
+                        let newEl = document.createElement("div");
+                        let id = item.getAttribute("id");
+                        newEl.classList.add("calc__estimate_info");
+                        newEl.setAttribute(`data-id`, `${id}`);
+                        newEl.innerHTML = `
+                        <p>${name}</p>
+                        <span>${item.value} тг</span>`;
+            
+                        calc__name.appendChild(newEl);
+                        calc__estimate.style.height = `${calc__estimate.clientHeight + 22}px`
+                        calc__result_box.style.height = `${calc__result_box.clientHeight + 22}px`
+                        prevstate = prevstate + 22;
+                    }
+                    else {
+                        let calc__estimate_info = document.querySelectorAll(".calc__estimate_info");
+                        let id = item.getAttribute("id")
+                        calc__estimate_info.forEach(count => {
+                            let dataId = count.dataset.id;
+                            if(dataId == id) {
+                                count.parentNode.removeChild(count);
+                                calc__estimate.style.height = `${calc__estimate.clientHeight - 22}px`
+                                calc__result_box.style.height = `${calc__result_box.clientHeight - 22}px`;
+                                prevstate = prevstate - 22;
+                            }
+                        })
+                    }
+                }
+            }
+        })
+
+    });
+    calc__item_range.forEach((item, ind1) => {
+        calc__item_number.forEach((count, ind2) => {
+            item.addEventListener("input", () => {
+                if(ind1 == ind2) {
+                    count.innerHTML = Number(item.value);
+                }
+            })
+        });
+        calc__item_check.forEach((count, ind2) => {
+            count.addEventListener("click", () => {
+                if(ind1 == ind2 && count.checked) {
+                    item.classList.remove("calc__range_active");
+                    item.removeAttribute("disabled")
+                }
+                else if(ind1 == ind2 && count.checked == false) {
+                    item.classList.add("calc__range_active");
+                    item.setAttribute("disabled", "disabled")
+                }
+            })
         })
     });
 
@@ -63,6 +185,7 @@ let calc = () => {
         });
         calc__ceck_range.forEach((count, ind2) => {
             count.addEventListener("click", () => {
+                console.log(ind1, ind2)
                 if(ind1 == ind2 && count.checked) {
                     item.classList.remove("calc__range_active");
                     item.removeAttribute("disabled")
@@ -127,44 +250,34 @@ let calc = () => {
         }
     });
 
+  
     calc__list_btn.addEventListener("click", () => {
         calc__list_btn.classList.toggle("active__btn");
-        calc__estimate.classList.toggle("acive__block")
+        if(calc__estimate.clientHeight > 0) {
+            calc__estimate.style.height = `${calc__estimate.clientHeight - calc__estimate.clientHeight}px`
+            calc__result_box.style.height = `${calc__result_box.clientHeight - calc__estimate.clientHeight}px`
+        }
+        else if (calc__estimate.clientHeight == 0) {
+            calc__estimate.style.height = `${prevstate}px`
+            calc__result_box.style.height = `${calc__result_box.clientHeight + prevstate}px`
+        }
     })
 
+    countResult = countResult + Number(calc__radiobox[1].value);
+    calc__result.innerHTML = `${countResult} тг`;
     calc__radiobox[0].addEventListener("click", () => {
         calc__radiobox[0].setAttribute("checked", "checked");
         calc__radiobox[1].removeAttribute("checked");
-        if(calc__radiobox[0].checked == true) {
-            countResult = countResult + Number(calc__radiobox[0].value);
-            calc__result.innerHTML = `${countResult} тг`;
-        }
-        else if(calc__radiobox[1].hasAttribute("checked") == false) {
-            if(countResult > 0) {
-                countResult = countResult - Number(calc__radiobox[1].value);
-                calc__result.innerHTML = `${countResult} тг`;
-            }
-            else {
-                return;
-            }
-        }
+        countResult = countResult + Number(calc__radiobox[0].value);
+        countResult = countResult - Number(calc__radiobox[1].value);
+        calc__result.innerHTML = `${countResult} тг`;
     })
     calc__radiobox[1].addEventListener("click", () => {
         calc__radiobox[1].setAttribute("checked", "checked");
         calc__radiobox[0].removeAttribute("checked");
-        if(calc__radiobox[1].checked == true) {
-            countResult = countResult + Number(calc__radiobox[1].value);
-            calc__result.innerHTML = `${countResult} тг`;
-        }
-        else if(calc__radiobox[0].hasAttribute("checked") == false) {
-            if(countResult > 0) {
-                countResult = countResult - Number(calc__radiobox[0].value);
-                calc__result.innerHTML = `${countResult} тг`;
-            }
-            else {
-                return;
-            }
-        }
+        countResult = countResult + Number(calc__radiobox[1].value);
+        countResult = countResult - Number(calc__radiobox[0].value);
+        calc__result.innerHTML = `${countResult} тг`;
     })
 }
 
